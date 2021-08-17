31c2c050459d05854b39a49dc58fc47a331ebdec
Henry-Azer <henaryazer@outlook.com>
2021-08-17 08:23:45 +0200

    Initial commit

---
+++ src/main/java/com/stores/DailyBalancesForStores/DailyBalancesForStoresApplication.java
@@ -0,0 +1,13 @@
+package com.stores.DailyBalancesForStores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBalancesForStoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBalancesForStoresApplication.class, args);
+	}
+
+}
